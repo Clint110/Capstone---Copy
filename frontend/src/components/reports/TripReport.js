51c20ff3c7e9ca44deb4efde0de7a50b2a7cd3fc
kylalinao <2101101943@student.buksu.edu.ph>
2024-04-13 00:42:24 +0800

    #sixthcommit

--- frontend/src/components/reports/TripReport.js
+++ frontend/src/components/reports/TripReport.js
@@ -1,25 +1,160 @@
-import React , {useEffect, useState} from 'react';
+import React, { useState, useEffect } from 'react';
+import jsPDF from 'jspdf';
+import 'jspdf-autotable';
+import axios from 'axios';
+import { Document, Page, Text, View, StyleSheet } from '@react-pdf/renderer';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleUser } from '@fortawesome/free-solid-svg-icons';
 import { FaHistory } from "react-icons/fa";
 import { IoDocumentAttachOutline } from "react-icons/io5";
-import axios from 'axios';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
+import logo from "../logo.png";
 import "primereact/resources/themes/lara-light-indigo/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
-
-
 
 function formatDateTime(dateTimeString) {
   const options = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', timeZoneName: 'short' };
   return new Date(dateTimeString).toLocaleDateString('en-US', options);
 }
 
-
-function TripReport() {
-
+const TripReport = () => {
   const [bookingData, setBookingData] = useState([]);
 
+  const generatePDF = () => {
+    try {
+      const doc = new jsPDF();
+  
+      doc.addImage(logo, 'PNG', 50, 15, 20, 18);
+
+      // doc.setFont('helvetica', 'bold'); // Set font to bold
+      doc.text('Bukidnon State University', 75, 25);
+      
+  
+      doc.setFontSize(10); // Adjust font size here
+      doc.text('Malaybalay City, Bukidnon', 86, 32);
+
+      doc.setFontSize(12); // Adjust font size here
+      doc.text('GSU - Motorpool Section', 83, 50);
+
+      doc.setFontSize(14); // Adjust font size here
+      doc.text('NUMBER OF TRIP VEHICLE FOR THE MONTH OF APRIL 2024', 35, 60);
+
+      doc.setFontSize(13); // Adjust font size here
+      doc.text('Within and Beyond Official Station', 72, 67);
+
+      const tableData = bookingData.map((booking, index) => [
+        booking.plateNumber,
+        booking.boundFor,
+        booking.destination,
+        formatDateTime(booking.timeForBound),
+        formatDateTime(booking.returnDate)
+      ]);
+
+      doc.autoTable({
+        startY: 87,
+        head: [['Plate Number', 'Bound For', 'Destination', 'Time For Bound', 'Return Date']],
+        body: tableData,
+        headStyles: {
+          fillColor: [255, 255, 255], // White background for header
+          textColor: [0, 0, 0],       // Black text color for header
+          lineColor: [0, 0, 0],       // Set header cell border color
+          lineWidth: 0.2,             // Set header cell border width
+          halign: 'center',           // Center align the header content horizontally
+          fontSize: 11,               // Adjust font size of the header
+          fontStyle: 'arialnarrow'    // Set font style to Arial Narrow
+        },
+        bodyStyles: {
+          fillColor: false,           // Remove background color for body cells
+          // fontStyle: 'bold',
+          // fontSize: 11, 
+          textColor: [0, 0, 0],       // Black text color for body
+          lineColor: [0, 0, 0],       // Set body cell border color
+          lineWidth: 0.2              // Set body cell border width
+        },
+        tableLineWidth: 0.2,          // Set table border width
+        tableLineColor: [0, 0, 0],    // Set table border color
+        margin: { top: 0 }            // Adjust table margin if needed
+      });
+      // Convert the PDF content into a data URL
+      const dataUri = doc.output('datauristring');
+
+      // Create a new HTML file with the PDF content
+      const htmlContent = `<html><head><title>Monthly Trip Report</title></head><body><iframe width="100%" height="100%" src="${dataUri}"></iframe></body></html>`;
+      const blob = new Blob([htmlContent], { type: 'text/html' });
+      const url = URL.createObjectURL(blob);
+
+      // Open the new HTML file in a new tab
+      window.open(url, '_blank');
+      
+    } catch (error) {
+      console.error('Error generating PDF:', error);
+    }
+  };
+
+  const handleGenerateReport = () => {
+    try {
+      const doc = new jsPDF();
+  
+      doc.addImage(logo, 'PNG', 50, 15, 20, 18);
+
+      // doc.setFont('helvetica', 'bold'); // Set font to bold
+      doc.text('Bukidnon State University', 75, 25);
+      
+  
+      doc.setFontSize(10); // Adjust font size here
+      doc.text('Malaybalay City, Bukidnon', 86, 32);
+
+      doc.setFontSize(12); // Adjust font size here
+      doc.text('GSU - Motorpool Section', 83, 50);
+
+      doc.setFontSize(14); // Adjust font size here
+      doc.text('NUMBER OF TRIP VEHICLE FOR THE MONTH OF APRIL 2024', 35, 60);
+
+      doc.setFontSize(13); // Adjust font size here
+      doc.text('Within and Beyond Official Station', 72, 67);
+
+      const tableData = bookingData.map((booking, index) => [
+        booking.plateNumber,
+        booking.boundFor,
+        booking.destination,
+        formatDateTime(booking.timeForBound),
+        formatDateTime(booking.returnDate)
+      ]);
+
+      doc.autoTable({
+        startY: 87,
+        head: [['Plate Number', 'Bound For', 'Destination', 'Time For Bound', 'Return Date']],
+        body: tableData,
+        headStyles: {
+          fillColor: [255, 255, 255], // White background for header
+          textColor: [0, 0, 0],       // Black text color for header
+          lineColor: [0, 0, 0],       // Set header cell border color
+          lineWidth: 0.2,             // Set header cell border width
+          halign: 'center',           // Center align the header content horizontally
+          fontSize: 11,               // Adjust font size of the header
+          fontStyle: 'arialnarrow'    // Set font style to Arial Narrow
+        },
+        bodyStyles: {
+          fillColor: false,           // Remove background color for body cells
+          // fontStyle: 'bold',
+          // fontSize: 11, 
+          textColor: [0, 0, 0],       // Black text color for body
+          lineColor: [0, 0, 0],       // Set body cell border color
+          lineWidth: 0.2              // Set body cell border width
+        },
+        tableLineWidth: 0.2,          // Set table border width
+        tableLineColor: [0, 0, 0],    // Set table border color
+        margin: { top: 0 }            // Adjust table margin if needed
+      });
+
+      // Save the PDF
+      doc.save('Monthly Trip Report.pdf');
+    } catch (error) {
+      console.error('Error generating PDF:', error);
+    }
+  };
   useEffect(() => {
     // Fetch booking data from the server
     const fetchBookingData = async () => {
@@ -38,61 +173,42 @@ function TripReport() {
     fetchBookingData();
   }, []);
 
-  const handleDocumentClick = () => {
-    // Define the functionality for handling the document click
-    console.error('Document clicked');
-  };
-
-  // const handleDocumentClick = () => {
-  //   // Pass bookingData to the report page
-  //   history.push(`/generate-report?bookingData=${JSON.stringify(bookingData)}`);
-  // };
-
   return (
-    <> 
-    
-    <div className="header-wrapper">
-<div className='header-container'>   
-        <h3><strong>REPORT</strong>    <p className="userName">Administrator</p>
-</h3>
-</div>
+    <>
+      <div>
+      <button onClick={generatePDF} className="generate-button">Generate Report</button>
+      </div>
+      <div className="header-wrapper">
+        <div className='header-container'>
+          <h4><strong>REPORT</strong>  </h4>   <span className="userName">Administrator <FontAwesomeIcon icon={faCircleUser} /></span>
 
-</div>
-<div style={{marginTop:"80px", float:"right", marginRight:"50px"}}>
-<button type="button" class="btn btn-success right">Generate Report</button>
-</div>
-<div className='Report-container'>
-    <div className='report-wrapper'>
-
-    <DataTable value={bookingData}
-        size="large"
-        showGridlines
-        border
-        removableSort
-        paginator rows={9}>
-            <Column field="plateNumber" header="Plate No."  />
+        </div>
+      </div>
+      <div className='Report-container'>
+        <div className='report-wrapper'>
+          <DataTable value={bookingData}
+            size="large"
+            showGridlines
+            removableSort
+            paginator rows={9}>
+            <Column field="plateNumber" header="Plate No." />
             <Column field="boundFor" header="Departure" />
             <Column field="destination" header="Destination" />
-            <Column field="timeForBound" header="Departure" body={(rowData) => formatDateTime(rowData.timeForBound)} sortable/>
-            <Column field="returnDate" header="Return" body={(rowData) => formatDateTime(rowData.returnDate)} sortable/>
+            <Column field="timeForBound" header="Departure" body={(rowData) => formatDateTime(rowData.timeForBound)} sortable />
+            <Column field="returnDate" header="Return" body={(rowData) => formatDateTime(rowData.returnDate)} sortable />
             <Column header="Action" body={() => (
               <React.Fragment>
                 <FaHistory className='actionBtn' />
-                <button onClick={handleDocumentClick} className='actionBtn btn-primary'>
-               <IoDocumentAttachOutline /> 
-               </button>
+               <button onClick={handleGenerateReport} className='actionBtn btn-primary'>
+                  <IoDocumentAttachOutline />
+                  </button>
               </React.Fragment>
             )} />
           </DataTable>
-          
-   
-      </div>
+        </div>
       </div>
     </>
+  );
+};
 
-
-
-  )
-}
-
-export default TripReport
+export default TripReport;
