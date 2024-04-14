d215996dad6c8a9f500f914148ddba7817596fc5
BrendylSingayao00 <2101101088@student.buksu.edu.ph>
2024-04-14 22:45:18 +0800

    small edit

--- frontend/src/components/reports/TripReport.js
+++ frontend/src/components/reports/TripReport.js
@@ -1,118 +1,144 @@
-import React, { useState, useEffect } from 'react';
-import jsPDF from 'jspdf';
-import 'jspdf-autotable';
-import axios from 'axios';
-import { Document, Page, Text, View, StyleSheet } from '@react-pdf/renderer';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleUser } from '@fortawesome/free-solid-svg-icons';
+import React, { useState, useEffect } from "react";
+import jsPDF from "jspdf";
+import "jspdf-autotable";
+import axios from "axios";
+import { Document, Page, Text, View, StyleSheet } from "@react-pdf/renderer";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleUser } from "@fortawesome/free-solid-svg-icons";
 import { FaHistory } from "react-icons/fa";
 import { IoDocumentAttachOutline } from "react-icons/io5";
-import { DataTable } from 'primereact/datatable';
-import { Column } from 'primereact/column';
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
 import logo from "../logo.png";
 import "primereact/resources/themes/lara-light-indigo/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
 
 function formatDateTime(dateTimeString) {
-  const options = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', timeZoneName: 'short' };
-  return new Date(dateTimeString).toLocaleDateString('en-US', options);
+  const options = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+    timeZoneName: "short",
+  };
+  return new Date(dateTimeString).toLocaleDateString("en-US", options);
 }
 
 const TripReport = () => {
   const [bookingData, setBookingData] = useState([]);
 
+  const [showEditModal, setShowEditModal] = useState(false);
+
+  const handleEditButtonClick = (plateNumber) => {
+    // Set the initial values for editing based on the selected vehicle's details
+    const selectedVehicle = vehicles.find(
+      (vehicle) => vehicle.plateNumber === plateNumber
+    );
+    if (selectedVehicle) {
+      setEditedPlateNumber(selectedVehicle.plateNumber);
+      setEditedVehicle(selectedVehicle.addvehicles);
+      setEditedCarImage(selectedVehicle.carImage);
+    }
+    setShowEditModal(true); // Open the edit modal
+  };
+
   const generatePDF = () => {
     try {
       const doc = new jsPDF();
-  
-      doc.addImage(logo, 'PNG', 50, 15, 20, 18);
+
+      doc.addImage(logo, "PNG", 50, 15, 20, 18);
 
       // doc.setFont('helvetica', 'bold'); // Set font to bold
-      doc.text('Bukidnon State University', 75, 25);
-      
-  
+      doc.text("Bukidnon State University", 75, 25);
+
       doc.setFontSize(10); // Adjust font size here
-      doc.text('Malaybalay City, Bukidnon', 86, 32);
+      doc.text("Malaybalay City, Bukidnon", 86, 32);
 
       doc.setFontSize(12); // Adjust font size here
-      doc.text('GSU - Motorpool Section', 83, 50);
+      doc.text("GSU - Motorpool Section", 83, 50);
 
       doc.setFontSize(14); // Adjust font size here
-      doc.text('NUMBER OF TRIP VEHICLE FOR THE MONTH OF APRIL 2024', 35, 60);
+      doc.text("NUMBER OF TRIP VEHICLE FOR THE MONTH OF APRIL 2024", 35, 60);
 
       doc.setFontSize(13); // Adjust font size here
-      doc.text('Within and Beyond Official Station', 72, 67);
+      doc.text("Within and Beyond Official Station", 72, 67);
 
       const tableData = bookingData.map((booking, index) => [
         booking.plateNumber,
         booking.boundFor,
         booking.destination,
         formatDateTime(booking.timeForBound),
-        formatDateTime(booking.returnDate)
+        formatDateTime(booking.returnDate),
       ]);
 
       doc.autoTable({
         startY: 87,
-        head: [['Plate Number', 'Bound For', 'Destination', 'Time For Bound', 'Return Date']],
+        head: [
+          [
+            "Plate Number",
+            "Bound For",
+            "Destination",
+            "Time For Bound",
+            "Return Date",
+          ],
+        ],
         body: tableData,
         headStyles: {
           fillColor: [255, 255, 255], // White background for header
-          textColor: [0, 0, 0],       // Black text color for header
-          lineColor: [0, 0, 0],       // Set header cell border color
-          lineWidth: 0.2,             // Set header cell border width
-          halign: 'center',           // Center align the header content horizontally
-          fontSize: 11,               // Adjust font size of the header
-          fontStyle: 'arialnarrow'    // Set font style to Arial Narrow
+          textColor: [0, 0, 0], // Black text color for header
+          lineColor: [0, 0, 0], // Set header cell border color
+          lineWidth: 0.2, // Set header cell border width
+          halign: "center", // Center align the header content horizontally
+          fontSize: 11, // Adjust font size of the header
+          fontStyle: "arialnarrow", // Set font style to Arial Narrow
         },
         bodyStyles: {
-          fillColor: false,           // Remove background color for body cells
+          fillColor: false, // Remove background color for body cells
           // fontStyle: 'bold',
-          // fontSize: 11, 
-          textColor: [0, 0, 0],       // Black text color for body
-          lineColor: [0, 0, 0],       // Set body cell border color
-          lineWidth: 0.2              // Set body cell border width
+          // fontSize: 11,
+          textColor: [0, 0, 0], // Black text color for body
+          lineColor: [0, 0, 0], // Set body cell border color
+          lineWidth: 0.2, // Set body cell border width
         },
-        tableLineWidth: 0.2,          // Set table border width
-        tableLineColor: [0, 0, 0],    // Set table border color
-        margin: { top: 0 }            // Adjust table margin if needed
+        tableLineWidth: 0.2, // Set table border width
+        tableLineColor: [0, 0, 0], // Set table border color
+        margin: { top: 0 }, // Adjust table margin if needed
       });
       // Convert the PDF content into a data URL
-      const dataUri = doc.output('datauristring');
+      const dataUri = doc.output("datauristring");
 
       // Create a new HTML file with the PDF content
       const htmlContent = `<html><head><title>Monthly Trip Report</title></head><body><iframe width="100%" height="100%" src="${dataUri}"></iframe></body></html>`;
-      const blob = new Blob([htmlContent], { type: 'text/html' });
+      const blob = new Blob([htmlContent], { type: "text/html" });
       const url = URL.createObjectURL(blob);
 
       // Open the new HTML file in a new tab
-      window.open(url, '_blank');
-      
+      window.open(url, "_blank");
     } catch (error) {
-      console.error('Error generating PDF:', error);
+      console.error("Error generating PDF:", error);
     }
   };
 
   const handleGenerateReport = () => {
     try {
       const doc = new jsPDF();
-  
-      doc.addImage(logo, 'PNG', 50, 15, 20, 18);
+
+      doc.addImage(logo, "PNG", 50, 15, 20, 18);
 
       // doc.setFont('helvetica', 'bold'); // Set font to bold
-      doc.text('Bukidnon State University', 75, 25);
-      
-  
+      doc.text("Bukidnon State University", 75, 25);
+
       doc.setFontSize(10); // Adjust font size here
-      doc.text('Malaybalay City, Bukidnon', 86, 32);
+      doc.text("Malaybalay City, Bukidnon", 86, 32);
 
       doc.setFontSize(12); // Adjust font size here
-      doc.text('GENERAL SERVICE UNIT', 83, 50);
+      doc.text("GENERAL SERVICE UNIT", 83, 50);
 
       doc.setFontSize(12); // Adjust font size here
-      doc.text('TRANSPORTATION SERVICE (Motorpool Section)', 83, 50);
-
-
+      doc.text("TRANSPORTATION SERVICE (Motorpool Section)", 69, 55);
 
       // doc.setFontSize(13); // Adjust font size here
       // doc.text('Within and Beyond Official Station', 72, 67);
@@ -122,52 +148,75 @@ const TripReport = () => {
       //   booking.boundFor,
       //   booking.destination,
       //   formatDateTime(booking.timeForBound),
-      //   formatDateTime(booking.returnDate)
+      //   formatDateTime(booking.returnDate),
       // ]);
+      const rowCount = 18;
+      const cellsPerRow = 4;
+      const tableData = [];
 
-      // doc.autoTable({
-      //   startY: 87,
-      //   head: [['Plate Number', 'Bound For', 'Destination', 'Time For Bound', 'Return Date']],
-      //   body: tableData,
-      //   headStyles: {
-      //     fillColor: [255, 255, 255], // White background for header
-      //     textColor: [0, 0, 0],       // Black text color for header
-      //     lineColor: [0, 0, 0],       // Set header cell border color
-      //     lineWidth: 0.2,             // Set header cell border width
-      //     halign: 'center',           // Center align the header content horizontally
-      //     fontSize: 11,               // Adjust font size of the header
-      //     fontStyle: 'arialnarrow'    // Set font style to Arial Narrow
-      //   },
-      //   bodyStyles: {
-      //     fillColor: false,           // Remove background color for body cells
-      //     // fontStyle: 'bold',
-      //     // fontSize: 11, 
-      //     textColor: [0, 0, 0],       // Black text color for body
-      //     lineColor: [0, 0, 0],       // Set body cell border color
-      //     lineWidth: 0.2              // Set body cell border width
-      //   },
-      //   tableLineWidth: 0.2,          // Set table border width
-      //   tableLineColor: [0, 0, 0],    // Set table border color
-      //   margin: { top: 0 }            // Adjust table margin if needed
-      // });
+      // Initialize the table with empty strings
+      for (let i = 0; i < rowCount; i++) {
+        const row = [];
+        for (let j = 0; j < cellsPerRow; j++) {
+          row.push("");
+        }
+        tableData.push(row);
+      }
+
+      // Now you can put different text inside each cell as needed
+      // For example, to set the text in the first cell of the first row:
+      tableData[0][0] = "Text for row 1, cell 1";
+      tableData[0][1] = "Text for row 1, cell 2";
+      tableData[0][2] = "Text for row 1, cell 2";
+      tableData[0][3] = "Text for row 1, cell 2";
+
+      // To set the text in the third cell of the fifth row:
+      tableData[4][2] = "Text for row 5, cell 3";
+
+      doc.autoTable({
+        startY: 87,
+        head: [["MOTOR VEHICLE USED REQUEST FORM"]],
+        body: tableData,
+
+        headStyles: {
+          fillColor: [255, 255, 255], // White background for header
+          textColor: [0, 0, 0], // Black text color for header
+          lineColor: [0, 0, 0], // Set header cell border color
+          lineWidth: 0.2, // Set header cell border width
+          halign: "center", // Center align the header content horizontally
+          fontSize: 11, // Adjust font size of the header
+          fontStyle: "arialnarrow", // Set font style to Arial Narrow
+        },
+        bodyStyles: {
+          fillColor: false, // Remove background color for body cells
+          // fontStyle: 'bold',
+          // fontSize: 11,
+          textColor: [0, 0, 0], // Black text color for body
+          lineColor: [0, 0, 0], // Set body cell border color
+          lineWidth: 0.2, // Set body cell border width
+        },
+        tableLineWidth: 0.2, // Set table border width
+        tableLineColor: [0, 0, 0], // Set table border color
+        margin: { top: 0 }, // Adjust table margin if needed
+      });
 
       // Save the PDF
-      doc.save('individual Report.pdf');
+      doc.save("individual Report.pdf");
     } catch (error) {
-      console.error('Error generating PDF:', error);
+      console.error("Error generating PDF:", error);
     }
   };
   useEffect(() => {
     // Fetch booking data from the server
     const fetchBookingData = async () => {
       try {
-        const response = await axios.get('http://localhost:3000/allbook'); // Replace with your actual API endpoint
+        const response = await axios.get("http://localhost:3000/allbook"); // Replace with your actual API endpoint
         const data = response.data;
 
         // Update the state with the fetched data
         setBookingData(data);
       } catch (error) {
-        console.error('Error fetching booking data:', error);
+        console.error("Error fetching booking data:", error);
       }
     };
 
@@ -178,52 +227,62 @@ const TripReport = () => {
   return (
     <>
       <div>
-      <button onClick={generatePDF} className="generate-button">Generate Report</button>
+        <button onClick={generatePDF} className="generate-button">
+          Generate Report
+        </button>
       </div>
       <div className="header-wrapper">
-        <div className='header-container'>
-          <h4><strong>REPORT</strong>  </h4>   <span className="userName">Administrator <FontAwesomeIcon icon={faCircleUser} /></span>
-
+        <div className="header-container">
+          <h4>
+            <strong>REPORT</strong>{" "}
+          </h4>{" "}
+          <span className="userName">
+            Administrator <FontAwesomeIcon icon={faCircleUser} />
+          </span>
         </div>
       </div>
       {/* <div className='Report-container'>
         <div className='report-wrapper'> */}
-        <div className='TableReportContainer'>
-      <table className='reportTable'>
-  <thead>
-    <tr>
-      <th>PLATE NO.</th>
-      <th>DEPARTURE</th>
-      <th>DESTINATION</th>
-      <th>DEPARTURE</th>
-      <th>RETURN</th>
-      <th>ACTION</th>
-     
-    </tr>
-  </thead>
-  <tbody>
-  {bookingData.map((booking) => (
-                  <tr key={booking._id}>
-                    <td>{booking.plateNumber}</td>
-                    <td>{booking.boundFor}</td>
-                    <td>{booking.destination}</td>
-                    <td>{formatDateTime(booking.timeForBound)}</td>
-                    <td>{formatDateTime(booking.returnDate)}</td>
-                
-                    <td>
-                      <button type="button" class="btn btn-warning btn-sm">Edit</button>&nbsp; 
-                      <button type="button" class="btn btn-danger btn-sm">Delete</button>
-                      <button onClick={handleGenerateReport} className='actionBtn '
-                      ><IoDocumentAttachOutline />
-                      </button>
-                    </td>
-                  </tr>
-                ))}
-  </tbody>
-</table>
+      <div className="TableReportContainer">
+        <table className="reportTable">
+          <thead>
+            <tr>
+              <th>PLATE NO.</th>
+              <th>DEPARTURE</th>
+              <th>DESTINATION</th>
+              <th>DEPARTURE</th>
+              <th>RETURN</th>
+              <th>ACTION</th>
+            </tr>
+          </thead>
+          <tbody>
+            {bookingData.map((booking) => (
+              <tr key={booking._id}>
+                <td>{booking.plateNumber}</td>
+                <td>{booking.boundFor}</td>
+                <td>{booking.destination}</td>
+                <td>{formatDateTime(booking.timeForBound)}</td>
+                <td>{formatDateTime(booking.returnDate)}</td>
+
+                <td>
+                  <button type="button" class="btn btn-warning btn-sm">
+                    Edit
+                  </button>
+                  &nbsp;
+                  <button type="button" class="btn btn-danger btn-sm">
+                    Delete
+                  </button>
+                  <button onClick={handleGenerateReport} className="actionBtn ">
+                    <IoDocumentAttachOutline />
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
 
-          {/* <DataTable value={bookingData}
+      {/* <DataTable value={bookingData}
             size="large"
             showGridlines
             removableSort
@@ -242,7 +301,7 @@ const TripReport = () => {
               </React.Fragment>
             )} />
           </DataTable> */}
-        {/* </div>
+      {/* </div>
       </div> */}
     </>
   );
