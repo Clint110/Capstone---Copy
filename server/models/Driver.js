ecc0a6b445793ec0bebf22a41d14e90e50bede59
Clint110 <tawoplays11@gmail.com>
2024-09-21 12:57:01 +0800

    driver

--- server/models/Driver.js
+++ server/models/Driver.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 const driverSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  status: { type: String, enum: ["currently driving", "active", "archived"], default: "available" } // Change "available" to "currently driving"
+  status: { type: String, enum: ["currently driving", "active", "archived"],
+   default: "active" } // Change "available" to "currently driving"
 });
 
 const Driver = mongoose.model("Driver", driverSchema);
